bc1ae9dc95ef67c5920458b223057095e7d02843
AleksandarIlicc <aleksandari336@gmail.com>
2022-05-05 22:54:52 +0200

    add new items in ProjectImages array

--- src/Arrays/images/ProjectImages.js
+++ src/Arrays/images/ProjectImages.js
@@ -13,10 +13,10 @@ const projectImages = [
         '/images/portfolio/portfolio--4.png',
         '/images/portfolio/portfolio--5.png'
     ],
-    // [
-    //     'images/components/react-form-validation--1.png',
-    //     'images/components/react-form-validation--2.png'
-    // ],
+    [
+        'images/components/react-form-validation--1.png',
+        'images/components/react-form-validation--2.png'
+    ],
     [
         '/images/building app/building-app--1.png',
         '/images/building app/building-app--2.png',
@@ -24,26 +24,26 @@ const projectImages = [
         '/images/building app/building-app--4.png',
         '/images/building app/building-app--5.png'
     ],
-    // [
-    //     '/images/components/slider--1.png',
-    //     '/images/components/slider--2.png'
-    // ],
-    // [
-    //     '/images/components/accordion--1.png',
-    //     '/images/components/accordion--2.png'
-    // ],
-    // [
-    //     '/images/components/animated-card--1.png',
-    //     '/images/components/animated-card--2.png'
-    // ],
-    // [
-    //     '/images/components/todo--1.png',
-    //     '/images/components/todo--2.png'
-    // ],
-    // [
-    //     '/images/components/travel-card--1.png',
-    //     '/images/components/travel-card--2.png'
-    // ],
+    [
+        '/images/components/slider--1.png',
+        '/images/components/slider--2.png'
+    ],
+    [
+        '/images/components/accordion--1.png',
+        '/images/components/accordion--2.png'
+    ],
+    [
+        '/images/components/animated-card--1.png',
+        '/images/components/animated-card--2.png'
+    ],
+    [
+        '/images/components/todo--1.png',
+        '/images/components/todo--2.png'
+    ],
+    [
+        '/images/components/travel-card--1.png',
+        '/images/components/travel-card--2.png'
+    ],
     [
         '/images/post-app/react-post--1.png',
         '/images/post-app/react-post--2.png',
