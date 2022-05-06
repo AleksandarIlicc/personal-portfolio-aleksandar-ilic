4bf2956f9003ff52bf0d0844392108c135789288
AleksandarIlicc <aleksandari336@gmail.com>
2022-05-07 00:36:15 +0200

    make responsive gallery

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -4,7 +4,7 @@ const PopUp = ({ showPopup, setShowPopup, showGalleryImages, projectName }) => {
     return (
         <div className={!showPopup ? "popup" : "popup popup__show"}>
             <div className="popup__window"> 
-                <div className="gallery">
+                <div className={showGalleryImages.length === 5 ? 'gallery gallery__website' : 'gallery gallery__component'}>
                     {
                         showGalleryImages.map((img, i) => (
                             <div className={`gallery__img gallery__img--${i + 1}`} >
